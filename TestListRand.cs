1e84abcfce799cbdc9aadd9715e3dff67311e34e
Vladislav <88485579+Bigibangi@users.noreply.github.com>
2022-01-17 23:03:10 +0300

    Add files via upload

---
+++ TestListRand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SerializeTestSaber;
+
+
+namespace TestListRandSaber
+{
+    [TestFixture]
+    class TestListRand
+    {
+
+        public ListRand CreateInstance()
+        {
+            return new ListRand() { Count = 0 };
+        }
+
+        [Test]
+        public void ListRandWithOneNode()
+        {
+            var listRand = CreateInstance();
+            var listNode = new ListNode();
+            listRand.AddNode(listNode);
+            Assert.AreEqual(listRand.Head, listNode);
+        }
+        [Test]
+        public void ListRandWithMoreThenOneNode()
+        {
+            var listRand = CreateInstance();
+            for (int i = 0; i < 5; i++)
+            {
+                listRand.AddNode(new ListNode());
+            }
+            Assert.AreEqual(5, listRand.Count);
+        }
+
+        [Test]
+        public void SerializeAndDeserializeListWithOneNode()
+        {
+            var listRand = new ListRand();
+            var listNode = new ListNode();
+            listNode.Data = "Test";
+            using (var fs = new FileStream("Test\\ListRandWithOneNode.txt", FileMode.OpenOrCreate))
+            {
+                listRand.AddNode(listNode);
+                listRand.Serialize(fs);
+            }
+            using (var fr = new FileStream("Test\\ListRandWithOneNode.txt", FileMode.Open))
+            {
+                listRand.Deserialize(fr);
+            }
+            Assert.AreEqual(listRand.Count, 1);
+            Assert.AreEqual(listRand.Head.Data, listNode.Data);
+        }
+    }
+}
